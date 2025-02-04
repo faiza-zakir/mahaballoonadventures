0d7a41ec9691d4c6c3851f8cc5b71215907668f5
YOUR NAMEcls <faiza.prism@gmail.com>
2025-02-04 16:47:10 +0500

    add lang in comp

--- app/components/Home/Banner/Banner.jsx
+++ app/components/Home/Banner/Banner.jsx
@@ -3,20 +3,12 @@ import { Container } from "react-bootstrap";
 import BookingField from "../../Common/BookingField/Index";
 import content from "./content";
 import "./styles.scss";
-import { usePathname } from "next/navigation";
 
 function Banner() {
-  const pathname = usePathname();
-  const locale = pathname.split("/")[1];
-
   return (
     <div className="homePageBanner">
       <Container>
-        <div
-          className={
-            locale == "en" ? "bannerWrapper" : "bannerWrapper bannerWrapperar"
-          }
-        >
+        <div className="bannerWrapper">
           <div className="content">
             <h1 className="bannerTitle desktopD3">{content?.title?.en}</h1>
             <h1 className="bannerTitle MObiled93">{content?.title?.en}</h1>
