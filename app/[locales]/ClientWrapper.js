ad2843087b7842abb0e8b656ec7e0355bda11386
YOUR NAMEcls <faiza.prism@gmail.com>
2025-02-06 16:34:21 +0500

    add gtags

--- app/[locales]/ClientWrapper.js
+++ app/[locales]/ClientWrapper.js
@@ -1,47 +1,3 @@
-// "use client";
-// import { usePathname } from "next/navigation";
-// import { ToastContainer } from "react-toastify";
-// import { Suspense, lazy } from "react";
-// import "react-toastify/dist/ReactToastify.css";
-// import Loader from "../components/Common/Loader/Loader";
-// import Providers from "../providers";
-// import { LanguageProvider } from "../context/LanguageContext";
-// const Navbar = lazy(() => import("../components/Layout/Navbar/Navbar"));
-// const Footer = lazy(() => import("../components/Layout/Footer/Footer"));
-
-// export default function ClientWrapper({ children }) {
-//   const pathname = usePathname();
-//   const locale = pathname.split("/")[1];
-
-//   return (
-//     <>
-//       <Providers>
-//         <LanguageProvider>
-//           <ToastContainer
-//             position="top-right"
-//             autoClose={5000}
-//             hideProgressBar={false}
-//             newestOnTop={false}
-//             closeOnClick
-//             rtl={false}
-//             pauseOnFocusLoss={false}
-//             draggable
-//             pauseOnHover={false}
-//             theme="light"
-//           />
-//           <Suspense fallback={<Loader />}>
-//             <Navbar />
-//             <main className={locale === "ar" ? "r_dir" : "l_dir"}>
-//               {children}
-//             </main>
-//             <Footer />
-//           </Suspense>
-//         </LanguageProvider>
-//       </Providers>
-//     </>
-//   );
-// }
-
 "use client";
 import { usePathname } from "next/navigation";
 import { ToastContainer } from "react-toastify";
@@ -73,11 +29,7 @@ export default function ClientWrapper({ children }) {
       />
       <Suspense fallback={<Loader />}>
         <Navbar />
-      </Suspense>
-      <main className={locale === "ar" ? "r_dir" : "l_dir"}>
-        <Suspense fallback={<Loader />}>{children}</Suspense>
-      </main>
-      <Suspense fallback={<Loader />}>
+        <main className={locale === "ar" ? "r_dir" : "l_dir"}>{children}</main>
         <Footer />
       </Suspense>
     </>
